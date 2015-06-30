f4744d546ebfde5d4665f99fc4516d036307634c
Jackon Yang <jiekunyang@gmail.com>
2015-06-30 21:24:02 +0800

    backup bookhub and leanread

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-python manage.py dumpdata --indent=4 base > base/fixtures/initial_data.json
+python manage.py dumpdata --indent=4 bookhub > bookhub/fixtures/initial_data.json
+python manage.py dumpdata --indent=4 leanread > leanread/fixtures/initial_data.json
 
 rm -rf init_data
 cp -R media init_data
